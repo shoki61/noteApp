04b774436e99d8039b3f9aacca99edf6bae91b7c
Sohrat <sohrat6128@gmail.com>
2020-07-10 16:26:25 +0300

    notu düzenleme kısmı yapıldı

--- src/styles/editNoteStyle.js
+++ src/styles/editNoteStyle.js
@@ -4,7 +4,23 @@ const styles = StyleSheet.create({
     editNoteContainer:{
         backgroundColor:'#fff',
         flex:1,
-
+    },
+    inputTitle:{
+        fontSize:23,
+        fontWeight:'bold',
+        textAlign:'center',
+        marginRight: 20,
+        marginLeft: 20,
+        marginTop: 25,
+        borderBottomWidth:2,
+        borderBottomColor:'#0BABC4',
+        color:'#0BABC4',
+    },
+    inputDesc:{
+        fontSize: 18,
+        color:'#6c6c6c',
+        paddingLeft:30,
+        paddingRight: 30,
     }
 
 })
--- src/views/pages/editNote.js
+++ src/views/pages/editNote.js
@@ -5,13 +5,34 @@ import SImage from 'react-native-scalable-image';
 import styles from '../../styles/editNoteStyle';
 
 class Edit_Note extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            title:'Lorem ipsum dolar sit amet',
+            desc:'Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nişi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+        }
+    }
     render(){
         return(
             <View style={styles.editNoteContainer}>
-                
+                <TextInput
+                    value={this.state.title}
+                    multiline={true}
+                    onChangeText={(text)=>this.setState({title:text})}
+                    style={styles.inputTitle}/>
+
+                <TextInput
+                    value={this.state.desc}
+                    multiline={true}
+                    onChangeText={(text)=>this.setState({desc:text})}
+                    style={styles.inputDesc}
+                />
+
             </View>
         )
     }
 }
 
 export default  Edit_Note;
+
+
