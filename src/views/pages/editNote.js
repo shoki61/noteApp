d762f216eeb959665cd4c47efdda414eaed0eaf4
Sohrat <sohrat6128@gmail.com>
2020-07-23 14:04:11 +0300

    bazı yerler düzeltildi

--- src/views/pages/editNote.js
+++ src/views/pages/editNote.js
@@ -1,47 +1,66 @@
 import React, { Component } from 'react';
-import {View, Text, TextInput, TouchableOpacity, ScrollView} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, ScrollView, Dimensions} from 'react-native';
 import SImage from 'react-native-scalable-image';
 import {observer} from 'mobx-react';
 
 import styles from '../../styles/editNoteStyle';
+import buttons from '../../styles/buttons';
 import controlData from '../../controllers/controlData';
 import saveData from '../../controllers/saveData';
-import AsyncStorage from '@react-native-community/async-storage';
 import helper from '../../controllers/helper';
+import saveDataAsyncStorage from '../../controllers/saveDataAsyncStorage';
+
+let originNote = []
+originNote.push({...controlData.showNote})
 
 class Edit_Note extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state={
+            title:controlData.showNote.title,
+            desc:controlData.showNote.desc,
+            showButton:false
+        }
+    }
+
+
+
     editNote(){
         saveData.userNotes[controlData.editNoteIndex]={
-            ...controlData.showNote,
+            ...this.state,
             date:helper.Date,
             time:helper.Time
         }
-        AsyncStorage.setItem('notes', JSON.stringify(saveData.userNotes))
+        saveDataAsyncStorage.saveNotes()
+        this.props.navigation.navigate('Notes')
     }
 
     render(){
         return(
             <View style={styles.editNoteContainer}>
                 <ScrollView>
-               <View>
+               <View style={{minHeight:Dimensions.get('window').height/2}}>
                    <TextInput
-                       value={controlData.showNote.title}
+                       value={this.state.title}
                        multiline={true}
-                       onChangeText={text=>controlData.showNote.title = text}
+                       onChangeText={text=> this.setState({title: text}) }
                        style={styles.inputTitle}/>
 
                    <TextInput
-                       value={controlData.showNote.desc}
+                       value={this.state.desc}
                        multiline={true}
-                       onChangeText={(text)=>controlData.showNote.desc = text}
+                       onChangeText={(text)=> this.setState({desc: text})}
                        style={styles.inputDesc}
                    />
                </View>
 
-                <TouchableOpacity onPress={()=>this.editNote()}>
-                    <Text>Değişikliği kaydet</Text>
-                </TouchableOpacity>
+                <View style={{alignItems:'center',marginTop:15}}>
+                    <TouchableOpacity onPress={()=>this.editNote()} style={buttons.saveButton}>
+                        <Text style={buttons.buttonText}>Değişikliği kaydet</Text>
+                    </TouchableOpacity>
+                </View>
+
                 </ScrollView>
 
             </View>
